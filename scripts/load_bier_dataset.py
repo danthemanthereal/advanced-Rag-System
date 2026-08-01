21bfced83bd8c6efcf367d4a3f5a5d96d07f88fb
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-08-01 22:41:00 +0200

    add script for loading bier dataset and store in parquet files

--- scripts/load_bier_dataset.py
+++ scripts/load_bier_dataset.py
@@ -1,1 +1,13 @@
-im
+from datasets import load_dataset
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parents[1] / "data"
+
+corpus = load_dataset("BeIR/fiqa", "corpus", split="corpus")
+queries = load_dataset("BeIR/fiqa", "queries", split="queries")
+qrels = load_dataset("BeIR/fiqa-qrels", split="test")
+
+corpus.to_parquet(DATA_DIR / "corpus.parquet")
+queries.to_parquet(DATA_DIR / "queries.parquet")
+qrels.to_parquet(DATA_DIR / "qrels.parquet")
+
