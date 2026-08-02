b431d3a3d317f0837ddd3de81695fbbcc2824a2d
Daniel Gontcharov <franzmeierderallerechte@gmail.com>
2026-08-02 13:10:14 +0200

    add eval component

---
+++ evaluator_component/evaluator.py
@@ -0,0 +1,15 @@
+import math
+
+class Evaluator:
+
+    def __init__(self,):
+        pass
+
+    def ndcg_at_k(self, predicted_ids: list[str], relevant: dict[str, int], k: int = 10):
+        dcg = sum(
+            relevant.get(doc_id, 0) / math.log2(rank + 2)
+            for rank, doc_id in enumerate(predicted_ids[:k])
+        )
+        ideal_rels = sorted(relevant.values(), reverse=True)[:k]
+        idcg = sum(rel / math.log2(rank + 2) for rank, rel in enumerate(ideal_rels))
+        return dcg / idcg if idcg > 0 else 0.0
--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from collections import defaultdict
 from data_loader_component.data_loader import DataLoader
 from pathlib import Path
 from tqdm import tqdm
-
+import numpy as np
+from evaluator_component.evaluator import Evaluator
 from reciprocal_rank_fusion.rrf import ReciprocalRankFusion
 from reranker_component.reranker import Reranker
 from retrieval.bm25_retriever import BM25Retriever
 from retrieval.dense_retriever import DenseRetriever
 from retrieval.hybrid_retriever import HybridRetriever
-from scripts.create_vector_db_index import dense_retriever
-
-"""SEED = 42
-RERANK_SAMPLE_SIZE=10
 
 DATA_DIR = Path(__file__).resolve().parents[0] / "data"
 
 data_loader = DataLoader()
+dense_retriever = DenseRetriever()
+bm25_retriever = BM25Retriever(data_loader=data_loader)
+rrf = ReciprocalRankFusion(60)
+hybrid_retriever = HybridRetriever(bm25_retriever=bm25_retriever,
+                                   dense_retriever=dense_retriever,
+                                   rrf=rrf)
+reranker = Reranker(
+    data_loader=data_loader,
+    model_name="cross-encoder/mmarco-mMiniLMv2-L12-H384-v1"
+)
+
+evaluator = Evaluator()
 
 queries = data_loader.get_data_as_pd(DATA_DIR / "queries.parquet")
 qrels_df = data_loader.get_data_as_pd(DATA_DIR / "qrels.parquet")
@@ -25,37 +34,28 @@ for _, row in qrels_df.iterrows():
     qrels[str(row["query-id"])][str(row["corpus-id"])] = int(row["score"])
 
 queries_with_qrels = queries[queries["_id"].astype(str).isin(qrels.keys())].copy()
-sample = queries_with_qrels.sample(n=RERANK_SAMPLE_SIZE, random_state=SEED)
 
-results: dict[str, list[float]] = defaultdict(list)
+sample = queries_with_qrels.head(50)
+
+results = []
 for _, row in tqdm(sample.iterrows(), total=len(sample), desc="Evaluating"):
     query_id = str(row["_id"])
     query_text = row["text"]
-    relevant = qrels[query_id]"""
+    relevant = qrels[query_id]
 
-data_loader = DataLoader()
-dense_retriever = DenseRetriever()
-bm25_retriever = BM25Retriever(data_loader=data_loader)
-rrf = ReciprocalRankFusion(60)
+    hybrid_rev = hybrid_retriever.get_top_k_hybrid_retrieval("Wie geht es dir", 10)
+    ids = [str(id) for id, _ in hybrid_rev]
+    res = reranker.rerank(query_text, ids, 5)
 
-hybrid_retriever = HybridRetriever(bm25_retriever=bm25_retriever,
-                                   dense_retriever=dense_retriever,
-                                   rrf=rrf)
-
-hybrid_rev = hybrid_retriever.get_top_k_hybrid_retrieval("Wie geht es dir", 10)
-ids = [str(id) for id, _ in hybrid_rev]
+    current_result = evaluator.ndcg_at_k(ids, relevant,5)
+    print("current result:", current_result )
+    results.append(current_result)
 
 
+print(f"Mean NDCG@5: {np.mean(results) * 100:.2f}")
 
-reranker = Reranker(
-    data_loader=data_loader,
-    model_name="cross-encoder/mmarco-mMiniLMv2-L12-H384-v1"
-)
 
-res = reranker.rerank("wieg geht es dir", ids, 5)
 
-for r in res:
-    print(r)
 
 
 
